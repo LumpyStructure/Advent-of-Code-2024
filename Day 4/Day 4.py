fef8544378aec8f1f514036dca7d402a04c1ef83
LumpyStructure <fraser@humph.net>
2024-12-22 22:51:56 +0000

    Add code for horizontals and verticals

--- Day 4/Day 4.py
+++ Day 4/Day 4.py
@@ -6,15 +6,33 @@
 
 def get_horizontals(path: str) -> list[str]:
     """Returns a list of the horizontals"""
+    with open(path, "r") as file:
+        horizontals = file.readlines()
+
+    # Remove the newline characters
+    for i in range(len(horizontals)):
+        horizontals[i] = horizontals[i].strip()
+    return horizontals
 
 
 def get_verticals(path: str) -> list[str]:
     """Returns a list of the verticals"""
+    horizontals = get_horizontals(path)
+
+    # Create a sublist for each vertical line (determined by the length of one of the horizontals)
+    verticals = [[] for _ in range(len(horizontals[0]))]
+
+    for horizontal in horizontals:
+        # Append each character in the horizontal to the next sublist in verticals
+        for i in range(len(horizontal)):
+            verticals[i].append(horizontal[i])
+
+    return verticals
 
 
 def get_diagonals(path: str) -> list[str]:
     """Returns a list of the diagonals"""
 
 
-def check_string(string: str) -> int:
+def check_string(string: list[str]) -> int:
     """Returns the number of occurences of 'XMAS', both forwards & backwards"""
