686d06a23e47433ea1fdeadbfc5d7188e585f362
LumpyStructure <fraser@humph.net>
2024-12-22 21:57:52 +0000

    Add code outline

---
+++ Day 4/Day 4.py
@@ -0,0 +1,20 @@
+# Ideas
+#   Rather than trying to reverse strings, maybe check for both 'XMAS' and 'SAMX'
+#   Do checking all by iterating, just pass in pre-processed strings for horizontal, verticals & diagonals
+#       If using this method, might be easier to just reverse string (or iterate in reverse order)
+
+
+def get_horizontals(path: str) -> list[str]:
+    """Returns a list of the horizontals"""
+
+
+def get_verticals(path: str) -> list[str]:
+    """Returns a list of the verticals"""
+
+
+def get_diagonals(path: str) -> list[str]:
+    """Returns a list of the diagonals"""
+
+
+def check_string(string: str) -> int:
+    """Returns the number of occurences of 'XMAS', both forwards & backwards"""
