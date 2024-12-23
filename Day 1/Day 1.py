1ec7b7cd26bbbe6abd8ce95aa38a9b4ca7eda7bf
LumpyStructure <fraser@humph.net>
2024-12-23 17:59:55 +0000

    Add comments for Day 1

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -1,7 +1,5 @@
-import math
-
-
 def get_file_data(path: str) -> tuple[list, list]:
+    """Reads the file at the given path and returns two lists"""
     with open(path, "r") as file:
         line_list = file.readlines()
     list_1 = []
@@ -15,30 +13,38 @@ def get_file_data(path: str) -> tuple[list, list]:
     return list_1, list_2
 
 
+# For part 1
 def find_difference(list_1: list[int], list_2: list[int]) -> int:
+    """Takes two lists and finds the total difference between corresponding elements in each list when sorted by size"""
     list_1.sort()
     list_2.sort()
+
+    # Calculate the absolute difference between corresponding elements in each list and sum the results
     total_difference = 0
     for i in range(len(list_1)):
-        total_difference += math.fabs(list_1[i] - list_2[i])
+        total_difference += abs(list_1[i] - list_2[i])
     return int(total_difference)
 
 
+# For part 2
 def find_similarity_score(list_1: list, list_2: list) -> int:
+    """Gives the similarity score of a list, defined as the number of occurences of a number in one list in the other list multiplied by the value of that number.
+    This is repeated for all numbers in the list and the sum is calculated."""
     list_1.sort()
     list_2.sort()
 
     similarity_score = 0
     for i in range(len(list_1)):
         list_1_num = list_1[i]
-        occurrences = 0
+        occurrences = 0  # Counts number of occurences of list_1_num in list_2
 
         try:
             list_2_index = list_2.index(list_1_num)
         except ValueError:
-            occurrences += 0
+            pass  # No occurences of list_1_num, so nothing needs to be done
         else:
             step = 0
+            # Iterate through list_2 until the current number is not equal to list_1_num
             while list_2[list_2_index + step] == list_1_num:
                 occurrences += 1
                 step += 1
@@ -48,7 +54,7 @@ def find_similarity_score(list_1: list, list_2: list) -> int:
     return similarity_score
 
 
-list_1, list_2 = get_file_data("Day 1 Puzzle Input.txt")
+list_1, list_2 = get_file_data("Advent-of-Code-2024\Day 1\Day 1 Puzzle Input.txt")
 difference = find_difference(list_1, list_2)
 similarity_score = find_similarity_score(list_1, list_2)
 print(difference)
